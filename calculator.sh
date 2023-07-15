077171580faa9fb705ece985d238df29cda0b13d
CaptainCluster <sranta.ville@gmail.com>
2023-07-15 22:06:30 +0300

    Added comments

--- calculator.sh
+++ calculator.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
+
+#This if-condition makes sure that the user inserts precisely
+#three arguments. The way these arguments work are explained
+#clearly in the README file.
 if [ "$#" != "3" ]; then
     echo "Expected number of arguments is 3, you gave $#."
     exit 0
 fi
 
+#These conditions make sure that the 2nd argument, the operator,
+#is a valid one. If the operator is +, it should obviously 
+#perform an addition. 
 if [ "$2" == "+" ]; then
     result=$(($1 + $3))
 
@@ -14,6 +21,7 @@ elif [ "$2" == "x" ]; then
     result=$(($1 * $3))
 
 elif [ "$2" == "/" ]; then
+    #We handle the "zero as a divider" exception here
 	if [ $3 -eq 0 ]; then
         echo "Can not divide with 0."
 	    exit 1
@@ -36,4 +44,5 @@ else
     exit -3
 fi
 
+#The calculation and its result will be displayed to the user
 echo "$1 $2 $3 = $result"
